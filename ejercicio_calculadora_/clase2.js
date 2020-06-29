22a8b41ab9121068a0343d7003b9882bb7797d9a
sebastian-cante <64110566+sebastian-cante@users.noreply.github.com>
2020-06-29 13:58:25 -0300

    Add files via upload

---
+++ ejercicio_calculadora_/clase2.js
@@ -0,0 +1,33 @@
+module.exports = {
+     sumar: function(numero1, numero2){
+        return numero1 + numero2;
+    },
+    
+    restar: function(numero1, numero2){
+        return numero1 - numero2;
+    },
+
+    multiplicar: function(numero1, numero2){
+        return numero1 * numero2;
+    },
+
+    dividir: function(numero1, numero2){
+        return numero1 / numero2;
+    }
+}
+
+function sumar(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function restar(numero1, numero2){
+    return numero1 - numero2;
+}
+
+function multiplicar(numero1, numero2){
+    return numero1 * numero2;
+}
+
+function dividir(numero1, numero2){
+    return numero1 / numero2;
+}
---
+++ ejercicio_calculadora_/index.js
@@ -0,0 +1,19 @@
+let calculadora = require('./clase2')
+
+//console.log( clase2.sumar(12, 34) );
+
+//console.log( clase2.restar(20, 4) );
+
+//console.log( clase2.multiplicar(2, 4) );
+
+//console.log( clase2.dividir(40, 2 );
+
+let suma = calculadora.sumar(3, 6);
+let resta = calculadora.restar(10, 2);
+let multiplicacion = calculadora.multiplicar(2, 3);
+let division = calculadora.dividir(30, 2);
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
